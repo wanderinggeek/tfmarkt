389d579be53868e663e89a2f134fc5a402a37035
Daniel Berg <dab@crosscan.com>
2017-05-05 17:55:53 +0200

    add produktklassen folder and change product class

---
+++ tfmarkt/Produktklassen/Produkt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tfmarkt.Produktklassen
+{
+    public abstract class Produkt
+    {
+        public double preis { get; set; }
+        public string name { get; set; }
+        public int artikelnummer { get; set; }
+        public string beschreibung { get; set; }
+
+    }
+}
